e1b9477974c89bf98d9b9fba969c7d495b75df95
Zachary Stence <zachstence@gmail.com>
2025-06-28 12:48:03 -0500

    chore: type d6_v13

--- src/lib/d6_v13/d6_v13.svelte
+++ src/lib/d6_v13/d6_v13.svelte
@@ -3,10 +3,11 @@
 	import { useGltf } from '@threlte/extras';
 	import { AutoColliders, RigidBody } from '@threlte/rapier';
 	import type { Group } from 'three';
+	import type { d6_v13_GLTF } from './d6_v13.types';
 
 	let props: Props<typeof Group> = $props();
 
-	const gltf = useGltf('/d6_v13.glb');
+	const gltf = useGltf<d6_v13_GLTF>('/d6_v13.glb');
 
 	const body = $derived($gltf?.nodes.Body);
 	const pips = $derived(
@@ -21,9 +22,11 @@
 		<RigidBody type="dynamic">
 			<AutoColliders shape="trimesh">
 				<T.Group dispose={false} rotation.x={Math.PI / 2} position={[-5, -5, -5]}>
-					<T.Mesh geometry={body.geometry}>
-						<T.MeshStandardMaterial color="red" />
-					</T.Mesh>
+					{#if body}
+						<T.Mesh geometry={body.geometry}>
+							<T.MeshStandardMaterial color="red" />
+						</T.Mesh>
+					{/if}
 					{#each pips as pip}
 						<T.Mesh geometry={pip.geometry}>
 							<T.MeshStandardMaterial color="white" />
--- src/lib/d6_v13/d6_v13.types.ts
+++ src/lib/d6_v13/d6_v13.types.ts
@@ -3,5 +3,27 @@ import * as THREE from 'three';
 export type d6_v13_GLTF = {
 	nodes: {
 		Body: THREE.Mesh;
+		Pip_1_1: THREE.Mesh;
+		Pip_2_1: THREE.Mesh;
+		Pip_2_2: THREE.Mesh;
+		Pip_3_1: THREE.Mesh;
+		Pip_3_2: THREE.Mesh;
+		Pip_3_3: THREE.Mesh;
+		Pip_4_1: THREE.Mesh;
+		Pip_4_2: THREE.Mesh;
+		Pip_4_3: THREE.Mesh;
+		Pip_4_4: THREE.Mesh;
+		Pip_5_1: THREE.Mesh;
+		Pip_5_2: THREE.Mesh;
+		Pip_5_3: THREE.Mesh;
+		Pip_5_4: THREE.Mesh;
+		Pip_5_5: THREE.Mesh;
+		Pip_6_1: THREE.Mesh;
+		Pip_6_2: THREE.Mesh;
+		Pip_6_3: THREE.Mesh;
+		Pip_6_4: THREE.Mesh;
+		Pip_6_5: THREE.Mesh;
+		Pip_6_6: THREE.Mesh;
 	};
+	materials: {};
 };
